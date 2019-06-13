6238077ea094e60d4c9b0a3125b6de8581fda547
Naushad Warsi <warsinak@gmail.com>
2019-06-13 17:01:42 +0530

    Got Multiple working but by creating new session and Link every single time

--- Producer/Program.cs
+++ Producer/Program.cs
@@ -10,38 +10,30 @@ namespace Producer
         {
 
             Console.WriteLine("Producer Started");
-            Sender();
-            Console.ReadLine();
-        }
-        private static async void Sender()
-        {
             try
             {
-
                 Address address = new Address("amqp://admin:admin@localhost:5672");
-                //Connection connection = await Connection.Factory.CreateAsync(address);
-
-                Connection connection = new Connection(new Address("amqp://admin:admin@localhost:5672"));
-
-                Session session = new Session(connection);
-
                 while (true)
                 {
-                Console.WriteLine("write something to send");
-                string msgstr = Console.ReadLine();
-                Message message = new Message(msgstr);
-                SenderLink sender = new SenderLink(session, "sender-link", "q1");
-                await sender.SendAsync(message);
+                    Connection connection = new Connection(new Address("amqp://admin:admin@localhost:5672"));
+
+                    Session session = new Session(connection);
+
+                    Console.WriteLine("write something to send");
+                    string msgstr = Console.ReadLine();
+                    Message message = new Message(msgstr);
+                    SenderLink sender = new SenderLink(session, "sender-link", "q1");
+                    sender.SendAsync(message);
 
                 }
-                
+
             }
             catch (Exception e)
             {
                 Console.WriteLine(e);
                 throw;
             }
-
+            Console.ReadLine();
         }
     }
 }
