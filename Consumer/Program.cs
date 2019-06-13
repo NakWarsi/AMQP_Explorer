6d169fd4061c381fddb3ef4185d7f6ac664c205e
Naushad Warsi <warsinak@gmail.com>
2019-06-13 17:20:56 +0530

    now working perfactly

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -17,14 +17,12 @@ namespace Consumer
         {
             try
             {
-                while (true)
-                {
-                   // var address = new Address("localhost", 5672, "admin", "admin");
-                //Connection connection = await Connection.Factory.CreateAsync(address);
                 Connection connection = new Connection(new Address("amqp://admin:admin@localhost:5672"));
                 Session session = new Session(connection);
 
-                ReceiverLink receiver = new ReceiverLink(session, "receiver-link", "q1");
+                ReceiverLink receiver = new ReceiverLink(session, "receiver-link2", "q1");
+                while (true)
+                {
                 Message message = await receiver.ReceiveAsync();
                 Console.WriteLine(message.Body.ToString());
                 receiver.Accept(message);
